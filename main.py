cb1238bed6c3e2f473cddb896a374e22fc2606db
rhuik159 <rhuik@naver.com>
2024-12-11 10:29:50 +0900

    CRUD:3rd

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+
+class Memo(BaseModel):
+    id:str
+    content:str
+
+memos = []
+
+app=FastAPI()
+
+@app.post("/memos")
+async def create_memo(memo:Memo):
+    memos.append(memo)
+    return '메모추가에 성공했습니다.'
+
+@app.get("/memos")
+async def read_memo():
+    return memos
+
+app.mount("/",StaticFiles(directory="static",html=True),name="static")
